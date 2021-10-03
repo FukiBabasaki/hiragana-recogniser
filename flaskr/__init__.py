4b1df62cb03ade5f0967ccf60c50ffc76ba85e93
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2021-10-03 16:03:44 +1300

    :tada: Initial commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,14 @@
+import os 
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    from . import pred
+    app.register_blueprint(pred.bp)
+
+    return app
 
 if __name__ == "__main__":
     pass
