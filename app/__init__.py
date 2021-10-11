9a9c66ea92ab15bd6d6dd94805ba2fa345f95880
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2021-10-12 01:42:29 +1300

    :sparkles: Change code for canvas drawing functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,12 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app_ = Flask(__name__, instance_relative_config=True)
 
-    from . import pred
-    app.register_blueprint(pred.bp)
+    from . import app
+    app_.register_blueprint(app.bp)
 
-    return app
+    return app_
 
 if __name__ == "__main__":
     pass
